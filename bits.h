9ab435badcc7210a3fd9e288c18d4464b9db6d98
99003694 <78849756+99003694@users.noreply.github.com>
2021-03-02 14:22:58 +0530

    Files added

---
+++ bits.h
@@ -0,0 +1,9 @@
+#ifndef __BITS_H
+#define __BITS_H
+#define SET 1
+#define NOTSET 0
+int set_bit(int a, int b);
+int clear_bit(int a, int b);
+int flip_bit(int a, int b);
+int query(int a, int b);
+#endif
---
+++ math.h
@@ -0,0 +1,9 @@
+#ifndef __MATH_H
+#define __MATH_H
+
+int factorial(int n1);
+int isprime(int n2);
+int isPalindrome(int n3);
+//int vsum(int n4,int n5,int n6,int n7 );
+int vsum(int num,...);
+#endif
